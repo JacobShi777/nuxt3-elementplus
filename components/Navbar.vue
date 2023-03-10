5273d023efb2f84fb7c6b92a99a5c27e7eb0dff3
Jacob <jacobshi777@hotmail.com>
2023-03-10 23:19:32 +0800

    feat: layouts, components

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    NavBar
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    Defalut Layout
+    <Navbar />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
