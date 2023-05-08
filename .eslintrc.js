65e2f8ceaddb2d7a624a337bcee197d1f716d418
Jacob <jacobshi777@hotmail.com>
2023-05-08 11:24:49 +0800

    chore: eslint的curly, max-statements-per-line配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     '@antfu/eslint-config',
   ],
   rules: {
-    'unused-imports/no-unused-imports': 'off',
+    'unused-imports/no-unused-imports': 'off', // 关闭未使用的 import 检测
+    'curly': ['error', 'multi-line'], // 允许单行 if 语句使用大括号
+    'max-statements-per-line': ['error', { max: 2 }], // 一行最多两个语句
   },
 }
