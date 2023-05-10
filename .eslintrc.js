bfb8ef44d46ec2cf15f12670741c62aa65a4f0e1
Jacob <jacobshi777@hotmail.com>
2023-05-10 10:55:22 +0800

    feat: 关闭@typescript-eslint/no-use-before-define

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,6 @@ module.exports = {
     'unused-imports/no-unused-imports': 'off', // 关闭未使用的 import 检测
     'curly': ['error', 'multi-line'], // 允许单行 if 语句使用大括号
     'max-statements-per-line': ['error', { max: 2 }], // 一行最多两个语句
+    '@typescript-eslint/no-use-before-define': 'off', // 允许定义前使用
   },
 }
